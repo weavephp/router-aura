cc7d193c3420d05e26272bf9671007b2662cda09
Robert Goldsmith <r.s.goldsmith@far-blue.co.uk>
2017-11-03 16:05:39 +0000

    Initial commit.

---
+++ src/Aura.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+/**
+ * Weave Router Adaptor for Aura.Router.
+ */
+namespace Weave\Router\Aura;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Aura\Router\RouterContainer;
+
+/**
+ * Weave Router Adaptor for Aura.Router.
+ */
+class Aura implements \Weave\Router\RouterAdaptorInterface
+{
+	/**
+	 * The Aura.Router routerContainer instance.
+	 *
+	 * @var RouterContainer
+	 */
+	protected $_routerContainer;
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct()
+	{
+		$this->_routerContainer = new RouterContainer();
+	}
+
+	/**
+	 * Using the provided callable, configure Aura.Router's routes.
+	 *
+	 * The routeProvider is called with Aura.Router's Map instance as the parameter.
+	 *
+	 * @param callable $routeProvider The method to use to configure the routes.
+	 *
+	 * @return null
+	 */
+	public function configureRoutes(callable $routeProvider)
+	{
+		$routeProvider($this->_routerContainer->getMap());
+	}
+
+	/**
+	 * Route the supplied request.
+	 *
+	 * @param Request $request The PSR7 request to attempt to route.
+	 *
+	 * @return false|array[Request, string|callable]
+	 */
+	public function route(Request $request)
+	{
+		$matcher = $this->_routerContainer->getMatcher();
+		$route = $matcher->match($request);
+		if (!$route) {
+			return false;
+		}
+
+		foreach ($route->attributes as $key => $val) {
+			$request = $request->withAttribute($key, $val);
+		}
+		return [$request, $route->handler];
+	}
+}
